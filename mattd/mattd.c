ac870292b0a7e7a0d98e4a34234f0c012b1f7eff
Kevin Conley <kevindconley@gmail.com>
2012-03-30 01:40:29 -0400

    successfully communicating with ardupilot

--- mattd/mattd.c
+++ mattd/mattd.c
@@ -16,74 +16,185 @@
 char *serial_path;
 int serial_fd;
 
+//stuff copied from EasyTransfer
+uint8_t rx_len;
+uint8_t rx_array_inx;
+
 void open_port() {
-  serial_fd = open(serial_path, O_RDWR | O_NOCTTY); //| O_NDELAY);
-  if (serial_fd == -1) {
-	  //Could not open serial port
-	  perror(DAEMON);
-	  exit(0);
-  } else
-	  fcntl(serial_fd, F_SETFL, 0); //block while reading
+	serial_fd = open(serial_path, O_RDWR | O_NOCTTY | O_NDELAY); //O_NDELAY: don't care if something's connected
+	if (serial_fd == -1) {
+		//Could not open serial port
+		perror(DAEMON);
+		exit(0);
+	} else
+		fcntl(serial_fd, F_SETFL, FNDELAY); //don't block while reading
 }
 
-char read_byte() {
-  char buf[1];
-  int val;
-  
-  val = read(serial_fd, buf, 1);
-  if (val > 0)
-    return buf[0];
-  else
-    return val;
+void close_port() {
+	close(serial_fd);
+}
+
+/* Read size bytes from serial, store in dest
+ * Return number of bytes available in serial buffer
+ * Daemon exits on error
+ */
+int read_bytes(uint8_t *dest, const int size) {
+	int val;
+
+	val = read(serial_fd, dest, size);
+	if (val >= 0) //include -1 for "resource temporarily unavailable"		
+		return val;
+	else {
+		printf("%d\n", val);
+		perror(DAEMON);
+		exit(0);
+	}
 } 
 
-/* buf is pointer to data
- * id is the type of data (see mattd.h)
+int available() {
+	uint8_t buf;
+	return read(serial_fd, &buf, 0);
+}
+
+/* Try to receive data and put it at location marked by dest
+ * Return size of data received, otherwise 0
+ */
+int receive_data(uint8_t* dest) {
+	uint8_t buf, calc_CS;
+	int i;
+
+	//try to receive preamble
+	if(rx_len == 0) {
+		if(available() >= 3) {
+			//this will block until a 0x06 is found or buffer size becomes less then 3.
+			read_bytes(&buf, 1);
+      			while(buf != 0x06) {
+				//This will trash any preamble junk in the serial buffer
+				//but we need to make sure there is enough in the buffer to process while we trash the rest
+				//if the buffer becomes too empty, we will escape and try again on the next call
+				if(available() < 3)
+					return 0;
+				read_bytes(&buf, 1);
+			}
+			read_bytes(&buf, 1);
+			if (buf == 0x85) {
+       		 		read_bytes(&rx_len, 1);
+				//malloc dest to this size
+				dest = (uint8_t *) malloc(rx_len);	
+      		 	}
+    		}
+	}
+
+	//we get here if we already found the header bytes
+	if(rx_len != 0){
+    		while(available() && rx_array_inx <= rx_len){
+      			read_bytes(dest + rx_array_inx++, 1);
+    		}
+    
+    		if(rx_len == (rx_array_inx - 1)){
+      			//seem to have got whole message
+      			//last uint8_t is CS
+      			calc_CS = rx_len;
+      			for (i = 0; i < rx_len; i++){
+        			calc_CS ^= dest[i];
+      			} 
+      
+      			if(calc_CS == dest[rx_array_inx - 1]){//CS good
+				rx_len = 0;
+				rx_array_inx = 0;
+				return rx_len;
+			} else {
+	  			//failed checksum, need to clear this out anyway
+				rx_len = 0;
+				rx_array_inx = 0;
+				return 0;
+	  		}
+    		}
+  	}
+	//TODO send NACK
+	buf = NACK;
+	write(serial_fd, &buf, 1); 
+	return 0;
+}
+
+/* data is pointer to data
  * len is number of bytes of the data
  */
-void send_data((const uint8_t *data, const int id, const int len) {
-  int i, x, y;
-  uint8_t buf;
-  //send data over serial
-  //first write preamble 
-  buf = 0x06;
-  write(serial_fd, &buf, 1);
-  buf = 0x85;
-  write(serial_fd, &buf, 1);
-  //then write size of data being sent
-  buf = len;
-  write(serial_fd, &buf, 1);
-  
-  //now send the data
-  for(i = 0; i < len; i++) {
-    CS ^= *(data + i);
-    buf = *(data + i);
-    write(serial_fd, &buf, 1);
-  }
-  //lastly, write checksum
-  buf = CS;
-  write(serial_fd, &buf, 1);
-  //TODO wait for ACK from Ardupilot, resend if ACK not received or NACK
+void send_data(const uint8_t *data, const int len) {
+	int i, j, val;
+	uint8_t buf, CS;
+
+	printf("max tries: %d", MAX_TRIES);	
+	//send data over serial
+	for(i = 0; i < MAX_TRIES; i++) {
+		//first write preamble 
+		printf("writing preamble...\n");
+		buf = 0x06;
+		write(serial_fd, &buf, 1);
+		buf = 0x85;
+		write(serial_fd, &buf, 1);
+
+		//then write size of data being sent
+		printf("writing size...\n");
+		buf = len;
+		write(serial_fd, &buf, 1);
+
+		//now send the data
+		printf("writing data...\n");
+		CS = len;
+		for(j = 0; j < len; j++) {
+			CS ^= *(data + j);
+			buf = *(data + j);
+			write(serial_fd, &buf, 1);
+		}
+		//lastly, write checksum
+		printf("writing checksum %x...\n", CS);
+		buf = CS;
+		write(serial_fd, &buf, 1);
+		//TODO wait for ACK from Ardupilot, resend if ACK not received or NACK
+		printf("waiting for ack...\n");
+		val = read_bytes(&buf, 1);	
+		if (buf == ACK) {
+			printf("Teddy got the data!\n");
+			break;
+		} else if (val == -1) {
+			//resource temporarily unavailable
+			printf("Resource unavailable, sending again, %dth try.\n", i);
+			if (i == MAX_TRIES) {
+				perror(DAEMON);
+				exit(0);		
+			}	
+		} else if (buf == NACK) {
+			i--;
+			continue;
+		} else {
+			printf("Teddy didn't get it, sending again, %dth try.\n", i);
+			if (i == MAX_TRIES) {
+				perror(DAEMON);
+				exit(0);		
+			}
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    serial_path = argv[1];
-  } else {
-    serial_path = DEFAULT_SERIAL_PORT;
-  }
-  
-  open_port();
-  
-  //TODO Start a thread for receiving data
-  
-  //TODO Start a thread for transmitting data to base station
-  
-  //TODO Start a thread for sending data to Ardupilot
-  
-  //TODO wait for threads to join?
-  
-  close(serial_fd);
-  
-  return(0);
+	if (argc > 1) {
+		serial_path = argv[1];
+	} else {
+		serial_path = DEFAULT_SERIAL_PORT;
+	}
+
+	printf("trying to open %s\n", serial_path);
+	open_port();
+
+	//test code
+	//char test_data[30] = "Hi Teddy!";
+
+	uint8_t test_data = 0xAB;
+	printf("trying to send byte %x\n", test_data);
+	send_data((uint8_t*) &test_data, 1);
+
+	close_port();
+
+	return(0);
 }
--- mattd/mattd.h
+++ mattd/mattd.h
@@ -1,7 +1,11 @@
 #ifndef MATTD_H
 #define MATTD_H
 
-#define DEFAULT_SERIAL_PORT         "/dev/ttyACM0"
-#define DAEMON                      "mattd"
+#define DEFAULT_SERIAL_PORT         	"/dev/ttyACM0"
+#define DAEMON                      	"mattd"
+#define MAX_TRIES			3
+#define NACK				0x00
+#define LAND				0xAA
+#define ACK				0xFF
 
 #endif
