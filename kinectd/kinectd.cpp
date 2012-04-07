3869c4807bd8ad793064f31c427d1de312707676
Paul Gurniak <pgurniak@gmail.com>
2012-04-07 17:37:19 -0400

    Fixed tearing issue

--- kinectd/kinectd.cpp
+++ kinectd/kinectd.cpp
@@ -63,7 +63,7 @@ int kinectInit(void)
   nRetVal = context.FindExistingNode(XN_NODE_TYPE_DEPTH, depth);
   depth.GetMetaData(depthMD);
 
-  nRetVal = depth.GetAlternativeViewPointCap().SetViewPoint(g_image);
+  //  nRetVal = depth.GetAlternativeViewPointCap().SetViewPoint(g_image);
   //nRetVal = depth.GetFrameSyncCap().FrameSyncWith(g_image);
 
  return nRetVal;
